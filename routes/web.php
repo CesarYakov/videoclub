cb21e2313118ea9e0ac1908e8d0c06a14caa68f3
CesarYakov <cesaryakov2020@gmail.com>
2024-05-23 23:23:59 -0500

    ejercicio1

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home',array('nombre'=>'César Escobedo'));
+});
+
+Route::get('/login', function(){
+    return view('auth.login');
+});
+
+Route::get('/logout', function(){
+    return redirect('/');
+});
+
+Route::get('/catalog', function(){
+    return view ('catalog.index');
+});
+
+Route::get('/catalog/show/{id}', function($id){
+    return view ('catalog.show',['id'=>$id]);
+});
+
+Route::get('/catalog/create', function(){
+    return view ('catalog.create');
+});
+
+Route::get('/catalog/edit/{id}', function($id){
+    return view ('catalog.edit',['id'=>$id]);
+});
+
+
+
+
+
+
