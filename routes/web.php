b705e2502a7f9f47cbd2783e4533a1130e4dd37d
CesarYakov <cesaryakov2020@gmail.com>
2024-05-23 23:26:27 -0500

    ejercicio1

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('Register', function () {
+    return view('home',array('nombre'=>'César Escobedo'));
+});
+
 
 Route::get('/', function () {
     return view('home',array('nombre'=>'César Escobedo'));
